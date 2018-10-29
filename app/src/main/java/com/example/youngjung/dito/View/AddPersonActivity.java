4656eaef384ebf6d4f028fb4028af756228b31f1
YoungK <quarl894@gmail.com>
2018-10-29 22:33:13 +0900

    1. 과제 수행자 지정하기 구현(Recyclerview 2개 + clickevent)

--- app/src/main/java/com/example/youngjung/dito/View/AddPersonActivity.java
+++ app/src/main/java/com/example/youngjung/dito/View/AddPersonActivity.java
@@ -6,24 +6,29 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.youngjung.dito.Adapter.Add1Adapter;
+import com.example.youngjung.dito.Adapter.Add2Adapter;
 import com.example.youngjung.dito.BaseActivity;
 import com.example.youngjung.dito.Model.member;
 import com.example.youngjung.dito.R;
 
 import java.util.ArrayList;
 
-public class AddPersonActivity extends BaseActivity{
+public class AddPersonActivity extends BaseActivity implements Add1Adapter.ItemClickListener{
     Toolbar toolbar;
     RecyclerView h_view;
     RecyclerView v_view;
-    RecyclerView.LayoutManager mlayoutManager;
+    RecyclerView.LayoutManager mlayoutManager, mlayoutManager2;
     ArrayList<member> members = new ArrayList<>();
+    ArrayList<member> show_member = new ArrayList<>();
+    member[] arr;
+    Add1Adapter add1Adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,16 +45,56 @@ public class AddPersonActivity extends BaseActivity{
 
         Intent i = getIntent();
         members = (ArrayList<member>) i.getExtras().get("mem");
+        arr = new member[members.size()+1];
 
         mlayoutManager = new LinearLayoutManager(this);
+        mlayoutManager2 = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
         v_view.setLayoutManager(mlayoutManager);
+        h_view.setLayoutManager(mlayoutManager2);
 
-        final Add1Adapter add1Adapter = new Add1Adapter(members,getApplicationContext());
+        add1Adapter = new Add1Adapter(members,getApplicationContext());
+        add1Adapter.setClickListener(this);
         v_view.setAdapter(add1Adapter);
 
-        add1Adapter.find();
+
     }
 
+    @Override
+    public void onItemClick(int pos, boolean chk, member person) {
+        ArrayList<member> m = new ArrayList<>();
+        if(chk){
+            arr[pos] = person;
+        }else{
+            arr[pos] = null;
+        }
+        for(member n : arr) {
+            if(n!=null){
+                m.add(n);
+                Log.e("add_arr", "" + n.getName());
+            }
+        }
+        Add2Adapter add2Adapter = new Add2Adapter(m,getApplicationContext());
+        //add2Adapter.setClickListener(this);
+        h_view.setAdapter(add2Adapter);
+    }
+
+    //add2adapter click event 위에 리사이클뷰 클릭시 삭제 보류
+//    @Override
+//    public void onItemClick(int pos, member person) {
+//        ArrayList<member> m = new ArrayList<>();
+//        arr[pos] = null;
+//        for(member n : arr) {
+//            if(n!=null){
+//                m.add(n);
+//               // Log.e("add_arr", "" + n.getName());
+//            }
+//        }
+//
+//        Add2Adapter add2Adapter = new Add2Adapter(m,getApplicationContext());
+//        add2Adapter.setClickListener(this);
+//        h_view.setAdapter(add2Adapter);
+//
+//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
